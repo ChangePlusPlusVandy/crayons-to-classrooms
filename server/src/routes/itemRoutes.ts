c285236c89398c3345b2ca49db9aafd2a9fad87d
Lyton <lytonm@lyton.dev>
2025-10-19 16:26:48 -0500

    add item routes

--- server/src/routes/itemRoutes.ts
+++ server/src/routes/itemRoutes.ts
@@ -1,6 +1,19 @@
-import { getItems } from '../controllers/itemController.js';
+import {
+  getItems,
+  getItemById,
+  createItem,
+  updateItem,
+  deleteItem,
+} from '../controllers/itemController.js';
 import express from 'express';
 
 export const itemsRoutes = express.Router();
 
 itemsRoutes.get('/', getItems);
+itemsRoutes.get('/:id', getItemById);
+
+itemsRoutes.post('/', createItem);
+
+itemsRoutes.put('/:id', updateItem);
+
+itemsRoutes.delete('/:id', deleteItem);
