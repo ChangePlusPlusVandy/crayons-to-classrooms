07c1cef4e4eeb30db3fb1bac2f7824e6028b5357
kevinchenbo2 <147973637+kevinchenbo2@users.noreply.github.com>
2025-10-31 23:57:10 -0500

    update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,6 +3,8 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import pool from './db.js';
 import testRoutes from './routes/testRoutes.js';
+import { itemsRoutes } from './routes/itemRoutes.js';
+import usersRoutes from './routes/usersRoutes.js';
 import inventoryMovementRoutes from './routes/inventoryMovementRoutes.js';
 
 dotenv.config();
@@ -16,22 +18,17 @@ app.get('/', (_req, res) => {
   res.send('Server is running');
 });
 
-// Mount inventory movement routes
-app.use('/api/inventory-movement', inventoryMovementRoutes);
 // Mount example test routes
 app.use('/api/test', testRoutes);
+
+app.use('/api/items', itemsRoutes);
+
+app.use('/api/users', usersRoutes);
+
+app.use('/api/inventory-movement', inventoryMovementRoutes);
 
 // Start server
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-<<<<<<< HEAD
-
-// Mount example test routes
-app.use('/api/test', testRoutes);
-// Mount inventory movement routes
-app.use('/api/inventory-movement', inventoryMovementRoutes);
-
-=======
->>>>>>> 471d8217b9faf39d767933b0d10ec652f16a1a80
