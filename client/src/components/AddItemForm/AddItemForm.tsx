377efe86a1fc6b52d4d97d422b3fc7a5d1c4cf21
kevinchenbo2 <147973637+kevinchenbo2@users.noreply.github.com>
2026-04-02 09:59:40 -0500

    fix prod bug issue

--- client/src/components/AddItemForm/AddItemForm.tsx
+++ client/src/components/AddItemForm/AddItemForm.tsx
@@ -131,7 +131,7 @@ export default function AddItemForm({
     }
 
     fetchInitialData();
-  }, []);
+  }, [initialItemInfoId, initialItemInfo]);
 
   // Compute warehouseLocations for form submission
   const warehouseLocations = useMemo(() => {
